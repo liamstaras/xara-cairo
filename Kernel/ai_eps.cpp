25f4244f3b90f073ad3927585b4a9272f8fef9fc
Thomas Hori <thomas.hori@liddicott.com>
2017-04-18 09:07:52 +0100

    Xara never released LX under "later versions" (which the GPL does not require although it suggests), therefore the concise notices resulting from exception removal are misleading.  Therefore fix them.
    Also, it's LX, not XL.

--- Kernel/ai_eps.cpp
+++ Kernel/ai_eps.cpp
@@ -1,12 +1,12 @@
 /*
  * xara-cairo, a vector drawing program
  *
- * Based on Xara XL, Copyright (C) 1993-2006 Xara Group Ltd.
+ * Based on Xara LX, Copyright (C) 1993-2006 Xara Group Ltd.
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 2 of the License, or (at your option)
- * any later version.
+ * Software Foundation, version 2.
+ *
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
